18b0b44011ab22b18524d19519edf64886f41ab2
Israth656 <israthbegum05@gmail.com>
2025-08-09 10:28:07 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,27 @@
+const WeatherData={
+    "Delhi":{
+        temp:39,
+        description:"Sunny and hot",
+        icon:"https://i.pinimg.com/564x/5b/04/93/5b0493b3a910f7ce7a737969bf3bc5d1.jpg"
+    },
+    "Mumbai":{
+        temp:30,
+        description:"Cloudy and humid",
+        icon:"https://static.vecteezy.com/system/resources/previews/038/044/854/non_2x/weather-forecast-partly-cloudy-icon-in-color-meteorology-overcast-vector.jpg"
+    },
+    "Chennai":{
+        temp:36,
+        description:"Sunny and hot",
+        icon:"https://cdn-icons-png.flaticon.com/512/1838/1838979.png"
+    },
+    "Banglore":{
+        temp:33,
+        description:"Thunderstrom expected",
+        icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJOSDpdFrdJBjfUvxNVz_GWl9uh7CRWJUhQA&s"
+    },
+    "Kolkata":{
+        temp:33,
+        description:"Thunderstrom expected",
+        icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJOSDpdFrdJBjfUvxNVz_GWl9uh7CRWJUhQA&s"
+    },
+}
